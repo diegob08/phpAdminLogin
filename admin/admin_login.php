5f8d376461294d884befef51fae0bacb7f80113d
Diego <d_bernalgomez@fanshaweonline.ca>
2018-02-14 16:22:52 -0500

    fixing issue login

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -13,12 +13,16 @@
     if($username !== "" && $password !== ""){ //checking that both are not empty
         //echo "you can type;)";
         $result = login($username, $password, $ip);
-        $message = $result;
+        //$message = $result;
+        redirect_to("admin_index.php");
+
     }else{
     $message = "Please fill in the required fields";
     //echo $message;
     }
   }
+
+
 ?>
 
 <!doctype html>
@@ -38,7 +42,7 @@
     <input type="text" name="username" value="" placeholder="username">
     <label class="hidden">Password:</label>
     <input type="password" name="password" value="" placeholder="password">
-    <input type="submit" name="submit" value="Login">
+    <input type="submit" name="submit" value="Sign In">
   </form>
 </section>
 </div>
