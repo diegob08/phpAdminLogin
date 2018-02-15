f109fdf269363b49982247f4ab209931c71ab7fe
Diego <31936806+diegob08@users.noreply.github.com>
2018-02-14 23:10:47 -0500

    Update admin_index.php
    added greeting

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -1,14 +1,28 @@
 <?php
-	require_once('phpscripts/config.php');
-	confirm_logged_in();
+require_once('phpscripts/config.php');
+confirm_logged_in();
 ?>
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Welcome to your admin panel</title>
+    <meta charset="UTF-8">
+    <title>Welcome to your admin panel</title>
 </head>
 <body>
-	<?php echo $_SESSION['user_name'];  ?>
+<?php
+$time = date("H");
+
+if ($time < "12") {
+    $greeting = "Good morning!";
+} else if ($time >= "12" && $time < "17") {
+    $greeting = "Good afternoon!";
+} else if ($time >= "17" && $time < "19") {
+    $greeting = "Good evening!";
+} else if ($time >= "19") {
+    $greeting = "Good night!";
+}
+?>
+<h3><?php echo $greeting . ' ' . $_SESSION['user_name']; ?></h3>
+<h5><?php echo 'Last Login in ' . $_SESSION['user_date']; ?></h5>
 </body>
 </html>
