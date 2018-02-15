12c638d119103f2330e05824d2dceeb84d9d9c52
Diego <31936806+diegob08@users.noreply.github.com>
2018-02-14 23:11:17 -0500

    Update login.php
    set up lock if failed

--- admin/phpscripts/login.php
+++ admin/phpscripts/login.php
@@ -1,28 +1,51 @@
 <?php
-function logIn($username, $password, $ip) {
-  require_once('connect.php');
-  $username = mysqli_real_escape_string($link, $username);
-  $password = mysqli_real_escape_string($link, $password);
-  $loginstring = "SELECT * FROM tbl_user WHERE user_name='{$username}' AND user_pass='{$password}'";
-  $userTest = mysqli_query($link, $loginstring);
-  echo mysqli_num_rows($userTest);
-  if(mysqli_num_rows($userTest)){
-    $founduser = mysqli_fetch_array($userTest, MYSQLI_ASSOC);
-    $id = $founduser['user_id'];
-    $_SESSION['user_id'] = $id;
-    $_SESSION['user_name']= $founduser['user_fname'];
-    if(mysqli_query($link, $loginstring)){
-      $update = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
-      $updatequery = mysqli_query($link, $update);
-      //date and time of LAST login
-      $lastTime = "UPDATE tbl_user SET user_date = NOW()  WHERE user_id = {$id}";
-      $showTime = mysqli_query($link,$lastTime);
+function logIn($username, $password, $ip)
+{
+    require_once('connect.php');
+    $username = mysqli_real_escape_string($link, $username);
+    $password = mysqli_real_escape_string($link, $password);
+    $password = md5($password);
+    $userQuery = "SELECT * FROM tbl_user WHERE user_name='{$username}'";
+    $user = mysqli_query($link, $userQuery);
+    if (mysqli_num_rows($user)) {
+        $founduser = mysqli_fetch_array($user, MYSQLI_ASSOC);
+        $id = $founduser['user_id'];
+        $user_attempt = $founduser['user_attempt'];
+        if ($user_attempt >= 3) {
+            return 'locked';
+        }
+        if ($founduser['user_pass'] === $password) {
+            $_SESSION['user_id'] = $id;
+            $_SESSION['user_name'] = $founduser['user_fname'];
+            $_SESSION['user_date'] = $founduser['user_date'];
+
+            $update = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
+            $updatequery = mysqli_query($link, $update);
+            //date and time of LAST login
+            $lastTime = "UPDATE tbl_user SET user_date = NOW()  WHERE user_id = {$id}";
+            $showTime = mysqli_query($link, $lastTime);
+
+            $resetAttemptQuery = "UPDATE tbl_user SET user_attempt = 0 WHERE user_id = {$id}";
+            $resetAttempt = mysqli_query($link, $resetAttemptQuery);
+            redirect_to("admin_index.php");
+        } else {
+            $user_attempt = 3 - $user_attempt - 1;
+            $IncreaseAttemptQuery = "UPDATE tbl_user SET user_attempt = user_attempt + 1 WHERE user_id={$id}";
+            $resetAttempt = mysqli_query($link, $IncreaseAttemptQuery);
+
+            $message = "Username and or password is incorrect. <br> Please make sure your caplock key is turned off.";
+            if ($user_attempt > 0) {
+                $message .= "You have {$user_attempt} times more before get locked.";
+            } else {
+                $message = 'locked';
+            }
+        }
+    } else {
+        $message = 'username does not exist!';
     }
-    redirect_to("admin_index.php");
-  }else{
-    $message = "Username and or password is incorrect. <br> Please make sure your caplock key is turned off.";
+
     return $message;
-  }
-  mysqli_close($link);
+    mysqli_close($link);
 }
+
 ?>
