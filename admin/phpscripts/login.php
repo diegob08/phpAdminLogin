2d81275970a131b22bbb599262b306a490b21755
Diego <d_bernalgomez@fanshaweonline.ca>
2018-02-15 20:19:19 -0500

    reset password

--- admin/phpscripts/login.php
+++ admin/phpscripts/login.php
@@ -4,22 +4,22 @@ function logIn($username, $password, $ip)
     require_once('connect.php');
     $username = mysqli_real_escape_string($link, $username);
     $password = mysqli_real_escape_string($link, $password);
-    $password = md5($password);
+  
     $userQuery = "SELECT * FROM tbl_user WHERE user_name='{$username}'";
     $user = mysqli_query($link, $userQuery);
 
     //adding attempts and date
     if (mysqli_num_rows($user)) {
-        $founduser = mysqli_fetch_array($user, MYSQLI_ASSOC);
-        $id = $founduser['user_id'];
-        $user_attempt = $founduser['user_attempt'];
+        $selectUser = mysqli_fetch_array($user, MYSQLI_ASSOC);
+        $id = $selectUser['user_id'];
+        $user_attempt = $selectUser['user_attempt'];//from database
         if ($user_attempt >= 3) { //maximum number of attemps
             return 'locked';
         }
-        if ($founduser['user_pass'] === $password) {
+        if ($selectUser['user_pass'] === $password) {
             $_SESSION['user_id'] = $id;
-            $_SESSION['user_name'] = $founduser['user_fname'];
-            $_SESSION['user_date'] = $founduser['user_date'];
+            $_SESSION['user_name'] = $selectUser['user_fname'];
+            $_SESSION['user_date'] = $selectUser['user_date'];
 
             $update = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
             $updatequery = mysqli_query($link, $update);
@@ -32,7 +32,7 @@ function logIn($username, $password, $ip)
             redirect_to("admin_index.php");
         } else {
             $user_attempt = 3 - $user_attempt - 1;
-            $IncreaseAttemptQuery = "UPDATE tbl_user SET user_attempt = user_attempt + 1 WHERE user_id={$id}"; 
+            $IncreaseAttemptQuery = "UPDATE tbl_user SET user_attempt = user_attempt + 1 WHERE user_id={$id}";
             $resetAttempt = mysqli_query($link, $IncreaseAttemptQuery);
 
             $message = "Username and or password is incorrect. <br> Please make sure your caplock key is turned off.";
